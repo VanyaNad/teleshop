fe631cc4a023cb8cdeb7978b132f69600cf00928
VanyaNad <vanyand2321@gmail.com>
2025-02-25 00:41:23 +0800

    refactor run bot to use async

--- bot/management/commands/run_telegram_bot.py
+++ bot/management/commands/run_telegram_bot.py
@@ -1,13 +1,13 @@
+import asyncio
 from django.core.management.base import BaseCommand
 from bot.telegram_bot import setup_bot
-from django.conf import settings
+from teleshop import settings
 
 
 class Command(BaseCommand):
     help = "Run the Telegram bot"
 
     def handle(self, *args, **kwargs):
-        updater = setup_bot()
+        app = setup_bot()  # Get bot instance
         self.stdout.write(self.style.SUCCESS(settings.BOT_IS_RUNNING))
-        updater.start_polling()
-        updater.idle()
+        asyncio.run(app.run_polling())
